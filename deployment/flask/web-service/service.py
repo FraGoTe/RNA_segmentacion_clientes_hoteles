a7568612d3f5500ec8ed8f5d7d7b3180f3fc5210
erodsan <31965887+erodsan@users.noreply.github.com>
2019-11-22 22:36:36 -0500

    Update service.py

--- deployment/flask/web-service/service.py
+++ deployment/flask/web-service/service.py
@@ -15,19 +15,19 @@ print ("Port recognized: ", port)
 app = Flask(__name__)
 CORS(app)
 #global loaded_model,loaded_scaler, graph loaded_model,loaded_scaler, graph = cargarModelo()
-global loaded_model,loaded_scaler, graph
-loaded_model,loaded_scaler, graph = cargarModelo()
+global loaded_model,loaded_scaler,loaded_labelEncoderX1,loaded_labelEncoderX2,loaded_labelEncoderX3,loaded_labelEncoderX4,loaded_labelEncoderX5,loaded_labelEncoderX6,loaded_labelEncoderX7, graph
+loaded_model,loaded_scaler,loaded_labelEncoderX1,loaded_labelEncoderX2,loaded_labelEncoderX3,loaded_labelEncoderX4,loaded_labelEncoderX5,loaded_labelEncoderX6,loaded_labelEncoderX7, graph = cargarModelo()
 
 #Define a route
 @app.route('/')
 def main_page():
 	return 'Modelo desplegado en la Nube!'
 
-@app.route('/enfermedad/', methods=['GET','POST'])
+@app.route('/hotel/', methods=['GET','POST'])
 def churn():
-	return 'Modelo de Deteccion de Diabetes!'
+	return 'Modelo de Segmentacion de Huespedes!'
 
-@app.route('/enfermedad/paciente/', methods=['GET','POST'])
+@app.route('/hotel/huesped/', methods=['GET','POST'])
 def default():
 	# print (request.data)
 	# print (request.args)
@@ -45,46 +45,60 @@ def default():
 	print("Data received:", data)
 
 	# Obteniendo parametros
-	Embarazos = data.get("Embarazos")
-	Glucosa = data.get("Glucosa")
-	Presion = data.get("Presion")
-	EspesorPiel = data.get("EspesorPiel")
-	Insulina = data.get("Insulina")
-	IMC = data.get("IMC")
-	DiabetesFamiliar = data.get("DiabetesFamiliar")
 	Edad = data.get("Edad")
+	Actividad_Laboral = data.get("Actividad_Laboral")
+	Nivel_Socioeconomico = data.get("Nivel_Socioeconomico")
+	Estado_Civil = data.get("Estado_Civil")
+	Nivel_Educacion = data.get("Nivel_Educacion")
+	Viaja_Solo = data.get("Viaja_Solo")
+	Medio_Reserva = data.get("Medio_Reserva")
+	Registros_Hotel = data.get("Registros_Hotel")
+	Tipo_Viaje = data.get("Tipo_Viaje")
 
-	print ("\nEmbarazos: ",Embarazos,
-			"\nGlucosa: ", Glucosa,
-			"\nPresion: ", Presion,
-			"\nEspesorPiel: ", EspesorPiel,
-			"\nInsulina: ", Insulina,
-			"\nIMC: ", IMC,
-			"\nDiabetesFamiliar: ", DiabetesFamiliar,
-			"\nEdad: ", Edad)
+	print ("\nEdad: ",Edad,
+			"\nActividad_Laboral: ", Actividad_Laboral,
+			"\nNivel_Socioeconomico: ", Nivel_Socioeconomico,
+			"\nEstado_Civil: ", Estado_Civil,
+			"\nNivel_Educacion: ", Nivel_Educacion,
+			"\nViaja_Solo: ", Viaja_Solo,
+			"\nMedio_Reserva: ", Medio_Reserva,
+	                "\nRegistros_Hotel: ", Registros_Hotel,
+			"\nTipo_Viaje: ", Tipo_Viaje)
 
 	# Transformado/Escalando la data
-	#[pais] = loaded_labelEncoderX1.transform([pais])
-	#[genero] = loaded_labelEncoderX2.transform([genero])
+	[Actividad_Laboral] = loaded_labelEncoderX1.transform([Actividad_Laboral])
+	[Nivel_Socioeconomico] = loaded_labelEncoderX2.transform([Nivel_Socioeconomico])
+	[Estado_Civil] = loaded_labelEncoderX3.transform([Estado_Civil])
+	[Nivel_Educacion] = loaded_labelEncoderX4.transform([Nivel_Educacion])
+	[Viaja_Solo] = loaded_labelEncoderX5.transform([Viaja_Solo])
+	[Medio_Reserva] = loaded_labelEncoderX6.transform([Medio_Reserva])
+	[Tipo_Viaje] = loaded_labelEncoderX7.transform([Tipo_Viaje])
 
-	paciente = np.array([Embarazos,Glucosa,Presion,EspesorPiel,Insulina,IMC,DiabetesFamiliar,Edad])
-	print("\npaciente: ", paciente)
-	paciente = loaded_scaler.transform([paciente])
-	print("cliente Norm: ", paciente)
+
+	huesped = np.array([Edad,Actividad_Laboral,Nivel_Socioeconomico,Estado_Civil,Nivel_Educacion,Viaja_Solo,Medio_Reserva,Registros_Hotel,Tipo_Viaje])
+	print("\nhuesped: ", huesped)
+	huesped = loaded_scaler.transform([huesped])
+	print("huesped Norm: ", huesped)
 
 	with graph.as_default():
 		resultado = ""
-		score = loaded_model.predict(paciente)
+		score = loaded_model.predict(huesped)
 		print("\nFinal score: ", score)
-		diabetico = (score > 0.5)
-		if diabetico:
-			resultado += "Diabetico"
-		else:
-		    resultado += "No Diabetico"
+		
+                if np.argmax(score) == 0: 
+			print(score[0][np.argmax(score)], ' --> Grupo I')
+			resultado += "Grupo I"
+		if np.argmax(score) == 1: 
+			print(score[0][np.argmax(score)], ' --> Grupo II')
+			resultado += "Grupo II"
+		if np.argmax(score) == 2: 
+			print(score[0][np.argmax(score)], ' --> Grupo III')
+			resultado += "Grupo III"
+		if np.argmax(score) == 3: 
+			print(score[0][np.argmax(score)], ' --> Grupo IV')
+			resultado += "Grupo IV"
+		
 		return resultado + ', score: ' + str(score[0])
-
-	# http://localhost:5000/abandono/cliente/?scoreCrediticio=3&pais=France&genero=Male&edad=36&tenencia=2&balance=1200.34&numDeProductos=3&tieneTarjetaCredito=1&esMiembroActivo=0&salarioEstimado=120000
-	# http://localhost:5000/abandono/cliente/?scoreCrediticio=1&pais=Spain&genero=Female&edad=50&tenencia=2&balance=200.34&numDeProductos=1&tieneTarjetaCredito=0&esMiembroActivo=0&salarioEstimado=85000
 
 # Run de application
 app.run(host='0.0.0.0',port=port)
