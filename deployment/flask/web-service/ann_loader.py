4328445d42eb174cdf2245a725d77d51ec971da1
erodsan <31965887+erodsan@users.noreply.github.com>
2019-11-23 01:54:10 -0500

    Update ann_loader.py

--- deployment/flask/web-service/ann_loader.py
+++ deployment/flask/web-service/ann_loader.py
@@ -9,12 +9,12 @@ def cargarModelo():
     FILENAME_MODEL_TO_LOAD = "model_tf_rn.h5"
     FILENAME_SCALER_TO_LOAD = "stdScaler.save"
     FILENAME_LABELENCODER_X1_TO_LOAD = "lab_enc_al.save"
-    FILENAME_LABELENCODER_X2_TO_LOAD = "lab_enc_ec.save"
-    FILENAME_LABELENCODER_X3_TO_LOAD = "lab_enc_mr.save"
+    FILENAME_LABELENCODER_X2_TO_LOAD = "lab_enc_ns.save"
+    FILENAME_LABELENCODER_X3_TO_LOAD = "lab_enc_ec.save"
     FILENAME_LABELENCODER_X4_TO_LOAD = "lab_enc_ne.save"
-    FILENAME_LABELENCODER_X5_TO_LOAD = "lab_enc_ns.save"
-    FILENAME_LABELENCODER_X6_TO_LOAD = "lab_enc_tv.save"
-    FILENAME_LABELENCODER_X7_TO_LOAD = "lab_enc_vs.save"
+    FILENAME_LABELENCODER_X5_TO_LOAD = "lab_enc_vs.save"
+    FILENAME_LABELENCODER_X6_TO_LOAD = "lab_enc_mr.save"
+    FILENAME_LABELENCODER_X7_TO_LOAD = "lab_enc_tv.save"
     MODEL_PATH = "../../../models/hotel"
 
     # Cargar la RNA desde disco
